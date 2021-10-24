8a204e7640a6186203d0c373ff8c58c0207e7353
Mary Yaschuk <feyaschuk@mail.ru>
2021-10-25 00:28:50 +0300

    flake8

--- gamers_email/settings.py
+++ gamers_email/settings.py
@@ -32,10 +32,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',    
+    'rest_framework',
     'esp',
     'game',
-    
+
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,5 +122,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
